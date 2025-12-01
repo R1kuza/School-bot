96741823f8d4669d9882feaadf620f29ae00534d
R1kuza <xudy1337@gmail.com>
2025-12-01 22:01:09 +0400

    Update

---
+++ check_size.py
@@ -0,0 +1,11 @@
+import os
+
+def get_size(start_path='.'):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            total_size += os.path.getsize(fp)
+    return total_size / (1024**3)  # GB
+
+print(f"Total size: {get_size():.2f} GB")
